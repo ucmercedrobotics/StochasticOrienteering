0ba348f83c1c0057d34bdd40e1b383a3ee84f254
Stefano Carpin <stefanocarpin@protonmail.com>
2023-12-01 16:59:21 -0800

    fixed plotting script

--- MCTS_StochasticOrienteering_TASE/visualize_graph.py
+++ MCTS_StochasticOrienteering_TASE/visualize_graph.py
@@ -32,6 +32,7 @@ file_list = glob.glob('../datasets/*.mat')
 for fname in file_list:
 
     og = graph.OrienteeringGraph(fname)
+    bn = os.path.basename(fname)
  
     NVERTICES = og.get_n_vertices()
     x = [og.vertices[i].x for i in og.vertices.keys()]
@@ -39,28 +40,32 @@ for fname in file_list:
 
   #  fig = plt.figure(figsize = (5,5))
     fig = plt.figure()
+    ax = plt.gca()
    # for i  in range(len(x)):
     plt.plot(x,y,'o',markersize=10)
-    if min(x) > 0:
-        plt.xlim([0.9*min(x),1.1*max(x)])
-    else:
-        plt.xlim([1.1*min(x),1.1*max(x)])
+    # if min(x) > 0:
+    #     plt.xlim([0.9*min(x),1.1*max(x)])
+    # else:
+    #     plt.xlim([1.1*min(x),1.1*max(x)])
         
-    if min(y) > 0:
-        plt.ylim([0.9*min(y),1.1*max(y)])
-    else:
-        plt.ylim([1.1*min(y),1.1*max(y)])    
+    # if min(y) > 0:
+    #     plt.ylim([0.9*min(y),1.1*max(y)])
+    # else:
+    #     plt.ylim([1.1*min(y),1.1*max(y)])    
+        
+    #ax.set_aspect('equal','box')
         
     #plt.ylim([0.95*min(y),1.05*max(y)])
     plt.rc('xtick', labelsize=20) 
     plt.rc('ytick', labelsize=20) 
-    #plt.axis('equal')
+    plt.axis('equal')
 
     plt.grid()
+    plt.title(bn)
     plt.plot(x[0],y[0],'ro')
     plt.plot(x[NVERTICES-1],y[NVERTICES-1],'ko')
     
-    bn = os.path.basename(fname)
+    
     
     fig.savefig(bn.rsplit( ".", 1 )[ 0 ] +'.pdf')
 #fig.tight_layout()
