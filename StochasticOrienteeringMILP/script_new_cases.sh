4409772b51ca270e9b2c5bb725ae99e6094280ab
Stefano Carpin <stefanocarpin@protonmail.com>
2023-12-12 14:55:15 -0800

    added st70 tests

--- StochasticOrienteeringMILP/script_new_cases.sh
+++ StochasticOrienteeringMILP/script_new_cases.sh
@@ -1,5 +1,6 @@
 python mcts_MILP.py --conf config_files/config_ulysses16_B50_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses16_50_005_120
 python mcts_MILP.py --conf config_files/config_ulysses16_B50_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses16_50_01_120
+
 python mcts_MILP.py --conf config_files/config_ulysses22_B50_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses22_50_005_120
 python mcts_MILP.py --conf config_files/config_ulysses22_B50_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses22_50_01_120
 
@@ -11,3 +12,6 @@ python mcts_MILP.py --conf config_files/config_att48_B25000_P01_Q120.txt --logdi
 
 python mcts_MILP.py --conf config_files/config_berlin52_B5000_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_005_120
 python mcts_MILP.py --conf config_files/config_berlin52_B5000_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_01_120
+
+python mcts_MILP.py --conf config_files/config_st70_B25000_P005_Q120.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_25000_005_120
+python mcts_MILP.py --conf config_files/config_st70_B25000_P01_Q120.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_25000_01_120
