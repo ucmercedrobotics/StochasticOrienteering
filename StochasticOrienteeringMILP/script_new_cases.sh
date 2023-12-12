1813cd9594c6e198e544636eec0b3010748e18b1
Stefano Carpin <stefanocarpin@protonmail.com>
2023-12-12 14:24:26 -0800

    added berlin52

--- StochasticOrienteeringMILP/script_new_cases.sh
+++ StochasticOrienteeringMILP/script_new_cases.sh
@@ -9,3 +9,5 @@ python mcts_MILP.py --conf config_files/config_burma14_B20_P01_Q120.txt --logdir
 python mcts_MILP.py --conf config_files/config_att48_B25000_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/att48_25000_005_120
 python mcts_MILP.py --conf config_files/config_att48_B25000_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/att48_25000_01_120
 
+python mcts_MILP.py --conf config_files/config_berlin52_B5000_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_005_120
+python mcts_MILP.py --conf config_files/config_berlin52_B5000_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_01_120
