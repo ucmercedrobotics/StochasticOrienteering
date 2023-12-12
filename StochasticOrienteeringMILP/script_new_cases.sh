3d35b65850f9ccdf63ccfe512cfd199f280acdf7
Stefano Carpin <stefanocarpin@protonmail.com>
2023-12-11 17:33:55 -0800

    updated new script

--- StochasticOrienteeringMILP/script_new_cases.sh
+++ StochasticOrienteeringMILP/script_new_cases.sh
@@ -1,3 +1,6 @@
 python mcts_MILP.py --conf config_files/config_ulysses16_50_005_alpha_Q80.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses_16_50_005
+python mcts_MILP.py --conf config_files/config_ulysses16_50_01_alpha_Q80.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses_16_50_01
+python mcts_MILP.py --conf config_files/config_ulysses22_50_005_alpha_Q80.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses_22_50_005
+python mcts_MILP.py --conf config_files/config_ulysses22_50_01_alpha_Q80.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses_22_50_01
 
 
