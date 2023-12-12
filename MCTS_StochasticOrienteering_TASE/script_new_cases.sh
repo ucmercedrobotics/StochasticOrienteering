9f343872f71f2bb5ebda28e3ab880964bd3c3df5
Stefano Carpin <stefanocarpin@protonmail.com>
2023-12-11 17:37:55 -0800

    configuration for new test cases

---
+++ MCTS_StochasticOrienteering_TASE/script_new_cases.sh
@@ -0,0 +1,7 @@
+python MCTS.py --conf config_files/config_ulysses16_50_005.txt --logdir MCTS`date "+%Y_%m_%d"`/ulysses_16_50_005
+python MCTS.py --conf config_files/config_ulysses16_50_01.txt --logdir MCTS`date "+%Y_%m_%d"`/ulysses_16_50_01
+python MCTS.py --conf config_files/config_ulysses22_50_005.txt --logdir MCTS`date "+%Y_%m_%d"`/ulysses_22_50_005
+python MCTS.py --conf config_files/config_ulysses22_50_01.txt --logdir MCTS`date "+%Y_%m_%d"`/ulysses_22_50_01
+
+
+
