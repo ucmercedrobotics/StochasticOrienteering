7a9d60aac3b947753219315e35f0b09f7a409260
Stefano Carpin <stefanocarpin@protonmail.com>
2023-12-12 10:41:03 -0800

    included burma testcase

--- StochasticOrienteeringMILP/script_new_cases.sh
+++ StochasticOrienteeringMILP/script_new_cases.sh
@@ -3,4 +3,7 @@ python mcts_MILP.py --conf config_files/config_ulysses16_B50_P01_Q120.txt --logd
 python mcts_MILP.py --conf config_files/config_ulysses22_B50_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses22_50_005_120
 python mcts_MILP.py --conf config_files/config_ulysses22_B50_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses22_50_01_120
 
+python mcts_MILP.py --conf config_files/config_burma14_B20_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/burma14_20_005_120
+python mcts_MILP.py --conf config_files/config_burma14_B20_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/burma14_20_01_120
+
 
