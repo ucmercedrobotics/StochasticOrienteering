d06d3621230fa8085419e8616e5fff3618a14323
Stefano Carpin <stefanocarpin@protonmail.com>
2023-12-12 10:27:22 -0800

    new filenames

--- StochasticOrienteeringMILP/script_new_cases.sh
+++ StochasticOrienteeringMILP/script_new_cases.sh
@@ -1,6 +1,6 @@
-python mcts_MILP.py --conf config_files/config_ulysses16_50_005_alpha_Q80.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses_16_50_005
-python mcts_MILP.py --conf config_files/config_ulysses16_50_01_alpha_Q80.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses_16_50_01
-python mcts_MILP.py --conf config_files/config_ulysses22_50_005_alpha_Q80.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses_22_50_005
-python mcts_MILP.py --conf config_files/config_ulysses22_50_01_alpha_Q80.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses_22_50_01
+python mcts_MILP.py --conf config_files/config_ulysses16_B50_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses16_50_005_120
+python mcts_MILP.py --conf config_files/config_ulysses16_B50_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses16_50_01_120
+python mcts_MILP.py --conf config_files/config_ulysses22_B50_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses22_50_005_120
+python mcts_MILP.py --conf config_files/config_ulysses22_B50_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses22_50_01_120
 
 
