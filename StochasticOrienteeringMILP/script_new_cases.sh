ecaad39eab6adea888ae9818eac9a8755e2019b1
Stefano Carpin <shamano@ucmerced-169-236-180-67.ucmerced.edu>
2023-12-21 14:31:15 -0800

    updated st70 config files

--- StochasticOrienteeringMILP/script_new_cases.sh
+++ StochasticOrienteeringMILP/script_new_cases.sh
@@ -13,5 +13,5 @@ python mcts_MILP.py --conf config_files/config_att48_B25000_P01_Q120.txt --logdi
 python mcts_MILP.py --conf config_files/config_berlin52_B5000_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_005_120
 python mcts_MILP.py --conf config_files/config_berlin52_B5000_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_01_120
 
-python mcts_MILP.py --conf config_files/config_st70_B25000_P005_Q120.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_25000_005_120
-python mcts_MILP.py --conf config_files/config_st70_B25000_P01_Q120.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_25000_01_120
+python mcts_MILP.py --conf config_files/config_st70_B500_P005_Q120.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_500_005_120
+python mcts_MILP.py --conf config_files/config_st70_B500_P01_Q120.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_500_01_120
