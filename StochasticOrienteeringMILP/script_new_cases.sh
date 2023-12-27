4c791a4b3b97e248ccc694567c59470feb2f3033
Stefano Carpin <stefanocarpin@protonmail.com>
2023-12-27 02:53:40 -0800

    updated

--- StochasticOrienteeringMILP/script_new_cases.sh
+++ StochasticOrienteeringMILP/script_new_cases.sh
@@ -1,17 +1,17 @@
 python mcts_MILP.py --conf config_files/config_ulysses16_B50_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses16_50_005_120
-python mcts_MILP.py --conf config_files/config_ulysses16_B50_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses16_50_01_120
+python mcts_MILP.py --conf config_files/config_ulysses16_B50_P01_Q200.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses16_50_01_120
 
 python mcts_MILP.py --conf config_files/config_ulysses22_B50_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses22_50_005_120
-python mcts_MILP.py --conf config_files/config_ulysses22_B50_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses22_50_01_120
+python mcts_MILP.py --conf config_files/config_ulysses22_B50_P01_Q200.txt --logdir MILP`date "+%Y_%m_%d"`/ulysses22_50_01_120
 
-python mcts_MILP.py --conf config_files/config_burma14_B20_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/burma14_20_005_120
-python mcts_MILP.py --conf config_files/config_burma14_B20_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/burma14_20_01_120
+python mcts_MILP.py --conf config_files/config_burma14_B10_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/burma14_20_005_120
+python mcts_MILP.py --conf config_files/config_burma14_B10_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/burma14_20_01_120
 
 python mcts_MILP.py --conf config_files/config_att48_B25000_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/att48_25000_005_120
-python mcts_MILP.py --conf config_files/config_att48_B25000_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/att48_25000_01_120
+python mcts_MILP.py --conf config_files/config_att48_B25000_P01_Q200.txt --logdir MILP`date "+%Y_%m_%d"`/att48_25000_01_120
 
 python mcts_MILP.py --conf config_files/config_berlin52_B5000_P005_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_005_120
-python mcts_MILP.py --conf config_files/config_berlin52_B5000_P01_Q120.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_01_120
+python mcts_MILP.py --conf config_files/config_berlin52_B5000_P01_Q200.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_01_120
 
 python mcts_MILP.py --conf config_files/config_st70_B500_P005_Q120.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_500_005_120
-python mcts_MILP.py --conf config_files/config_st70_B500_P01_Q120.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_500_01_120
+python mcts_MILP.py --conf config_files/config_st70_B500_P01_Q200.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_500_01_120
