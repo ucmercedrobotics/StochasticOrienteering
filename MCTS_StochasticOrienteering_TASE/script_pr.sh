a2c176f0229a9be3376668ec295f8010eeede00a
Stefano Carpin <stefanocarpin@protonmail.com>
2024-01-08 06:19:29 -0800

    tweaked script

--- MCTS_StochasticOrienteering_TASE/script_pr.sh
+++ MCTS_StochasticOrienteering_TASE/script_pr.sh
@@ -1,5 +1,6 @@
 for i in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9;
 do
+    rm -rf config_files/result
     grep RANDOM  config_files/config_pr.txt > config_files/result
     if [ -s config_files/result ] ; then
         sed -i '' -e '$ d' config_files/config_pr.txt
