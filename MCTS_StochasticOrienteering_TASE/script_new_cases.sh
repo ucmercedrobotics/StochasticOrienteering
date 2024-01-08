054e0b7e37ff29f24e34477fe7830a62d96cc84e
Stefano Carpin <stefanocarpin@protonmail.com>
2024-01-08 01:24:58 -0800

    fixed bug

--- MCTS_StochasticOrienteering_TASE/script_new_cases.sh
+++ MCTS_StochasticOrienteering_TASE/script_new_cases.sh
@@ -4,16 +4,16 @@ python MCTS.py --conf config_files/config_ulysses16_B50_P01.txt --logdir MCTS`da
 python MCTS.py --conf config_files/config_ulysses22_B50_P005.txt --logdir MCTS`date "+%Y_%m_%d"`/ulysses_22_50_005
 python MCTS.py --conf config_files/config_ulysses22_B50_P01.txt --logdir MCTS`date "+%Y_%m_%d"`/ulysses_22_50_01
 
-python MILP.py --conf config_files/config_burma14_B10_P005.txt --logdir MILP`date "+%Y_%m_%d"`/burma14_10_005
-python MILP.py --conf config_files/config_burma14_B10_P01.txt --logdir MILP`date "+%Y_%m_%d"`/burma14_10_01
+python MCTS.py --conf config_files/config_burma14_B10_P005.txt --logdir MILP`date "+%Y_%m_%d"`/burma14_10_005
+python MCTS.py --conf config_files/config_burma14_B10_P01.txt --logdir MILP`date "+%Y_%m_%d"`/burma14_10_01
 
-python MILP.py --conf config_files/config_att48_B25000_P005.txt --logdir MILP`date "+%Y_%m_%d"`/att48_25000_005
-python MILP.py --conf config_files/config_att48_B25000_P01.txt --logdir MILP`date "+%Y_%m_%d"`/att48_25000_01
+python MCTS.py --conf config_files/config_att48_B25000_P005.txt --logdir MILP`date "+%Y_%m_%d"`/att48_25000_005
+python MCTS.py --conf config_files/config_att48_B25000_P01.txt --logdir MILP`date "+%Y_%m_%d"`/att48_25000_01
 
-python MILP.py --conf config_files/config_berlin52_B5000_P005.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_005 
-python MILP.py --conf config_files/config_berlin52_B5000_P01.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_01
+python MCTS.py --conf config_files/config_berlin52_B5000_P005.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_005 
+python MCTS.py --conf config_files/config_berlin52_B5000_P01.txt --logdir MILP`date "+%Y_%m_%d"`/berlin52_5000_01
 
-python MILP.py --conf config_files/config_st70_B500_P005.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_500_005
-python MILP.py --conf config_files/config_st70_B500_P01.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_500_01
+python MCTS.py --conf config_files/config_st70_B500_P005.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_500_005
+python MCTS.py --conf config_files/config_st70_B500_P01.txt  --logdir MILP`date "+%Y_%m_%d"`/st70_500_01
 
 
